65d1cdd9a3e6b1bb5cfc59a50ced33415a167f87
Huang yao ting <a0916996002@gmail.com>
2023-08-01 23:53:21 +0800

    Add: Increasing Triplet Subsequence, Reverse Vowels of a String

--- leetcode_QA/Reverse Vowels of a String.py
+++ leetcode_QA/Reverse Vowels of a String.py
@@ -18,9 +18,38 @@ Example 2:
 
 class Solution(object):
     def reverseVowels(self, s):
-        """     
+        """
         :type s: str
         :rtype: str
         """
+        vowels = []
+        compare = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        for i in range(len(s)):
+            if s[i] in compare:
+                vowels.append(s[i])
+
+        vowels.reverse()
+
+        k = 0
+        result = ""
+
+        for i in range(len(s)):
+            if s[i] in compare:
+                result += vowels[k]
+                k += 1
+            else:
+                result += s[i]
+
+        return result
 
 
+solution = Solution()
+
+s = "hello"
+result = solution.reverseVowels(s)
+print(result)
+
+s = "leetcode"
+result = solution.reverseVowels(s)
+print(result)
